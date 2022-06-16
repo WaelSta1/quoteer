294901e5d73f56a14adb9c0c7840cbefcbafb969
WaelSta1 <waelsta055@gmail.com>
2022-06-16 14:48:06 +0100

    added a spinner and finished the project

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ const quoteText = document.getElementById('quote')
 const quoteAuthor = document.getElementById('author')
 const newQuoteBtn = document.getElementById('new-quote')
 const twitterBtn = document.getElementById('twitter')
+const loader = document.getElementById('loader')
+const quoteContainer = document.getElementById('quote-container')
+//show Loader
+const loading = ()=>{
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loader
+const complete = () =>{
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // getting the quotes from the api
 const getQuotes = async ()=>{
+    loading()
     try{
         const response = await fetch("https://type.fit/api/quotes")
         allQuotes = await response.json() 
@@ -13,10 +27,12 @@ const getQuotes = async ()=>{
     }catch(exception){
         console.log(exception);
     }
+    complete()
 }
 
 // getting one random quote from the response
 const randomQuote =()=>{
+    loading()
     const quote = allQuotes[Math.floor(Math.random()*allQuotes.length)];
     quoteText.textContent = quote.text;
     
@@ -26,12 +42,14 @@ const randomQuote =()=>{
     }else{
         quoteText.classList.remove('long-quote')
     }
+    complete()
 }
 
 //onLoad
 getQuotes()
 .then(()=>randomQuote())
 
+console.log(getQuotes);
 const tweetQuote = ()=>{
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`
     window.open(twitterUrl,"_blank")
