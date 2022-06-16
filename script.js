40bf0707d59b5b00dbbdc69082c484d90066d89c
WaelSta1 <waelsta055@gmail.com>
2022-06-16 14:20:16 +0100

    added the tweet functionality and did some code refactoring

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-// getting the quotes from the api
 let allQuotes = [];
+const quoteText = document.getElementById('quote')
+const quoteAuthor = document.getElementById('author')
+const newQuoteBtn = document.getElementById('new-quote')
+const twitterBtn = document.getElementById('twitter')
 
+// getting the quotes from the api
 const getQuotes = async ()=>{
     try{
         const response = await fetch("https://type.fit/api/quotes")
-        allQuotes = await response.json()
-        console.log(allQuotes);
-        return allQuotes;
-
+        allQuotes = await response.json() 
+        console.log(allQuotes)     
     }catch(exception){
         console.log(exception);
     }
 }
 
 // getting one random quote from the response
-
-const getRandomQuote = ()=>{
-    const quoteNumber = Math.floor(Math.random()*allQuotes.length);
-    return allQuotes[quoteNumber]
-
+const randomQuote =()=>{
+    const quote = allQuotes[Math.floor(Math.random()*allQuotes.length)];
+    quoteText.textContent = quote.text;
+    
+    quote.author? quoteAuthor.textContent = quote.author : quoteAuthor.textContent = 'Unknown'
+    if(quote.text.length>100){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote')
+    }
 }
 
-// displaying the quote and the author
-const displayQuote = ()=>{
-    const quote = getRandomQuote();
-    document.getElementById("quote").innerHTML = quote.text;
-    document.getElementById("author").innerHTML = quote.author;
+//onLoad
+getQuotes()
+.then(()=>randomQuote())
+
+const tweetQuote = ()=>{
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`
+    window.open(twitterUrl,"_blank")
 }
+
+
+//event listeners
+newQuoteBtn.addEventListener('click',randomQuote)
+twitterBtn.addEventListener('click',tweetQuote)
